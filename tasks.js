57f19f87b433cd239fcd3635eef956619e74ea47
Anton Konan <Anton.Konan@itechart-group.com>
2019-05-02 16:26:51 +0300

    'tasks'

---
+++ tasks.js
@@ -0,0 +1,74 @@
+// var a = 0;
+// var b = 0;
+
+// function partApp(parameters, callback) {
+// console.log('ayy');
+// }
+
+// partApp(a,b);
+
+// function callb() {
+
+// }
+
+// function test(par1, par2) {
+//     console.log(arguments);
+// }
+
+
+//task 5
+
+function newMap(array, callback) {
+    var newArray = [];
+    for(var i = 0; i < array.length; i++) {
+        newArray[i] = callback(array[i]);
+    }
+    return newArray;
+}
+
+//task6
+
+function newFilter(array, callback) {
+    var newArray = [];
+    for(var i = 0; i < array.length; i++) {
+        if(callback(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+
+//task3 ?
+
+function newReduce(array, callback, initialValue) {
+    var prevElem = initialValue;
+    for(var i = 0; i < array.length; i++) {        
+        prevElem = callback(prevElem, array[i], i, array);
+    }
+    return prevElem;
+}
+
+//task 7
+
+var array = [1,23,2,6,12,0];
+
+var evenNumbersSum = newReduce(array, function(accum, item) {
+    if(item % 2 == 0) {
+        accum.sum = accum.sum + item;
+        accum.count = accum.count + 1;
+    }
+    return accum;
+}, {sum: 0, count: 0});
+
+var answer = evenNumbersSum.sum / evenNumbersSum.count;
+console.log(answer);
+
+//task 9
+
+function filterFirstElem(array, callback) {
+    for(var i = 0; i < array.length; i++) {
+        if(callback(array[i])) {
+            return array[i];
+        }
+    }
+}

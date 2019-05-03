a935fa803bc68ceb12c564a8bb80a88769067426
Anton K <ntnkonon@gmail.com>
2019-05-03 17:46:23 +0300

    multiple tasks added

--- tasks.js
+++ tasks.js
@@ -64,12 +64,110 @@ function partApp(parameter, func) {
 }
 
 
+//task1 for multiple params
 
-// //task1 for multiple params
-// function partApp2() {
-//     var args = [...arguments];
-//     var func = arguments[arguments.length - 1];
-//     for(var i = 0; i < arguments.length; i++) {
-        
-//     }
-// }
+function partApp2(func) {
+    if(arguments.length > 1) {
+        var boundArgs = arguments.slice(1);
+        return function() {
+            return func.apply(null, boundArgs.concat(arguments))
+        }
+    }
+}
+
+// task 11
+
+function memoization(func) {
+    var cache = {};
+    return function() {
+        var arg = arguments[0];
+        if (cache[arg]) {
+            return cache[arg];
+        }
+        else {           
+            var f = func.call(null, arg);
+            cache[arg] = f;
+            return f;
+        }
+    }    
+}
+
+//task4 w/o generator
+
+function unfold(callback, value) {
+    var result = [];
+    var current = value;
+    var done = false;    
+    var {current, next, done} = callback(current);
+    while(!done) {  
+        result.push(current);      
+        var {current, next, done} = callback(next);                
+    }
+    return result;
+}
+
+//task4 test 
+console.log(unfold(
+    (n) => n > 0 
+    ? {
+        current: n,
+        next: n - 1
+    }
+    : { done: true}
+, 5));
+
+
+//task4 w/ generator
+
+function unfoldGenerator(callback, value) {
+    var result = [];
+    var current = value;
+    function * gen(value) {
+        var {current, next, done} = callback(value);
+        if(!done) {
+            result.push(current);            
+            yield * gen(next); 
+        }        
+    }
+    var unfold = gen(current);
+    unfold.next();
+    return result;
+}
+
+//task 4 w/ generator test
+
+
+console.log(unfoldGenerator(
+    (n) => n > 0 
+    ? {
+        current: n,
+        next: n - 1
+    }
+    : { done: true}
+, 5));
+
+
+
+//task8
+
+var counter = 0;
+
+var randomSum = unfold(
+    function(n) {
+        if(counter < 10) {
+            counter = counter + 1;
+            return {
+                current: n,
+                next: Math.floor(Math.random() * 100)
+            }
+        }
+        else {
+            return {
+                done: true
+            }
+        }                
+    }, Math.floor(Math.random() * 100));
+
+console.log(newReduce(randomSum, function(accum, item) {
+    return accum + item;
+}, 0));
